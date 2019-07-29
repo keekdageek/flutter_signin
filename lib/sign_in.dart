035d05d4c93006c69ec2d88992edf9b692730303
Francisco Marin <francisco.marin@personalcapital.com>
2019-07-28 22:50:58 -0700

    better first screen

--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -4,6 +4,10 @@ import 'package:google_sign_in/google_sign_in.dart';
 final FirebaseAuth _auth = FirebaseAuth.instance;
 final GoogleSignIn googleSignIn = GoogleSignIn();
 
+String name;
+String email;
+String imageUrl;
+
 Future<String> signInWithGoogle() async {
   final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
   final GoogleSignInAuthentication googleSignInAuthentication =
@@ -15,6 +19,17 @@ Future<String> signInWithGoogle() async {
   );
 
   final FirebaseUser user = await _auth.signInWithCredential(credential);
+  assert(user.email != null);
+  assert(user.displayName != null);
+  assert(user.photoUrl != null);
+
+  name = user.displayName;
+  email = user.email;
+  imageUrl = user.photoUrl;
+
+  if (name.contains(" ")) {
+    name = name.substring(0, name.indexOf(" "));
+  }
 
   assert(!user.isAnonymous);
   assert(await user.getIdToken() != null);
